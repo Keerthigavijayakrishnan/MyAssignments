e38d8dc88f4666f8fbb01940c4cef9f576e93245
Keerthigavijayakrishnan <110728322+Keerthigavijayakrishnan@users.noreply.github.com>
2022-08-06 20:14:22 +0530

    Add files via upload
    Day1

---
+++ day1/twowheeler.java
@@ -0,0 +1,34 @@
+package week1.day1;
+
+public class twowheeler {
+
+	int noOfWheels = 2;
+	short noOfMirrors = 2;
+	long chassisNumber = 202050036;
+	boolean isPunchured = false;
+	String bikeName = "RTR";
+	double runningKM = 160d;
+	
+     public static void main(String[] args) {
+     
+    	 int noOfWheels = 2;
+    	 System.out.println(noOfWheels);
+    	 
+    		short noOfMirrors = 2;
+    		System.out.println(noOfMirrors);
+    		
+    		long chassisNumber = 202050036;
+    		System.out.println(chassisNumber);
+    		
+    		boolean isPunchured = false;
+    		 System.out.println(isPunchured);
+    		 
+    		 String bikeName = "RTR";
+       		 System.out.println(bikeName);
+    		 
+    		double runningKM = 160d;
+    		System.out.println(runningKM);
+    	 
+    	 
+	}
+}
